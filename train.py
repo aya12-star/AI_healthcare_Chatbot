032cd3cf28a910e5fea8068a06c9070a90d3dfb3
Aya <mabroukiaya2@gmail.com>
2024-12-09 20:15:28 +0100

    add

---
+++ train.py
@@ -0,0 +1,18 @@
+from chatterbot import ChatBot  # type: ignore
+from chatterbot.trainers import ListTrainer # type: ignore
+import os
+
+try:
+    os.remove("db.sqlite3")
+    print("Old database removed. Training new database")
+except :
+    print('No database found. Creating new database.')
+
+english_bot = ChatBot('Bot')
+english_bot.set_trainer(ListTrainer)
+
+for file in os.listdir('data'):
+    print('Training using ' + file)
+    convData = open('data/' + file).readlines()
+    english_bot.train(convData)
+    print("Training completed for " + file)
